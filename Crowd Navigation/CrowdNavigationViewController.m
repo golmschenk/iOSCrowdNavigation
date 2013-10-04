26d18e123a60797b0047f359bbb80cd2bc9b832e
Greg Olmschenk <golmschenk@gmail.com>
2013-10-04 13:53:50 -0400

    Fix directional audioplayers

--- Crowd Navigation/CrowdNavigationViewController.m
+++ Crowd Navigation/CrowdNavigationViewController.m
@@ -12,6 +12,10 @@
 @interface CrowdNavigationViewController ()
 - (IBAction)getDirection:(id)sender;
 @property (weak, nonatomic) IBOutlet UILabel *displayDirection;
+@property AVAudioPlayer *leftPlayer;
+@property AVAudioPlayer *rightPlayer;
+@property AVAudioPlayer *forwardPlayer;
+@property AVAudioPlayer *stopPlayer;
 
 @end
 
@@ -20,6 +24,54 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self preparePlayers];
+}
+
+- (void)preparePlayers
+{
+    NSError *playerError;
+    
+    NSString *leftPath =
+    [[NSBundle mainBundle] pathForResource: @"left"
+                                    ofType: @"mp3"];
+    
+    NSURL *leftURL = [[NSURL alloc] initFileURLWithPath: leftPath];
+    
+    self.leftPlayer =
+    [[AVAudioPlayer alloc] initWithContentsOfURL: leftURL
+                                           error: &playerError];
+
+    NSString *rightPath =
+    [[NSBundle mainBundle] pathForResource: @"right"
+                                    ofType: @"mp3"];
+    
+    NSURL *rightURL = [[NSURL alloc] initFileURLWithPath: rightPath];
+    
+    self.rightPlayer =
+    [[AVAudioPlayer alloc] initWithContentsOfURL: rightURL
+                                           error: &playerError];
+    
+    NSString *forwardPath =
+    [[NSBundle mainBundle] pathForResource: @"forward"
+                                    ofType: @"mp3"];
+    
+    NSURL *forwardURL = [[NSURL alloc] initFileURLWithPath: forwardPath];
+    
+    self.forwardPlayer =
+    [[AVAudioPlayer alloc] initWithContentsOfURL: forwardURL
+                                           error: &playerError];
+    
+    NSString *stopPath =
+    [[NSBundle mainBundle] pathForResource: @"stop"
+                                    ofType: @"mp3"];
+    
+    NSURL *stopURL = [[NSURL alloc] initFileURLWithPath: stopPath];
+    
+    self.stopPlayer =
+    [[AVAudioPlayer alloc] initWithContentsOfURL: stopURL
+                                           error: &playerError];
+    
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -29,62 +81,24 @@
 }
 
 - (void) playLeft{
-    NSString *soundFilePath =
-    [[NSBundle mainBundle] pathForResource: @"left"
-                                    ofType: @"mp3"];
-    
-    NSURL *fileURL = [[NSURL alloc] initFileURLWithPath: soundFilePath];
-    NSError *requestError;
-    
-    AVAudioPlayer *newPlayer =
-    [[AVAudioPlayer alloc] initWithContentsOfURL: fileURL
-                                           error: &requestError];
-    
-    [newPlayer play];
+    [self.leftPlayer play];
 }
 
 - (void) playRight{
-    NSString *soundFilePath =
-    [[NSBundle mainBundle] pathForResource: @"right"
-                                    ofType: @"mp3"];
-    
-    NSURL *fileURL = [[NSURL alloc] initFileURLWithPath: soundFilePath];
-    
-    AVAudioPlayer *newPlayer =
-    [[AVAudioPlayer alloc] initWithContentsOfURL: fileURL
-                                           error: nil];
-    [newPlayer play];
+    [self.rightPlayer play];
 }
 
 - (void) playForward{
-    NSString *soundFilePath =
-    [[NSBundle mainBundle] pathForResource: @"forward"
-                                    ofType: @"mp3"];
-    
-    NSURL *fileURL = [[NSURL alloc] initFileURLWithPath: soundFilePath];
-    
-    AVAudioPlayer *newPlayer =
-    [[AVAudioPlayer alloc] initWithContentsOfURL: fileURL
-                                           error: nil];
-    [newPlayer play];
+    [self.forwardPlayer play];
 }
 
 - (void) playStop{
-    NSString *soundFilePath =
-    [[NSBundle mainBundle] pathForResource: @"stop"
-                                    ofType: @"mp3"];
-    
-    NSURL *fileURL = [[NSURL alloc] initFileURLWithPath: soundFilePath];
-    
-    AVAudioPlayer *newPlayer =
-    [[AVAudioPlayer alloc] initWithContentsOfURL: fileURL
-                                           error: nil];
-    [newPlayer play];
+    [self.stopPlayer play];
 }
 
 - (IBAction)getDirection:(id)sender {
-    //NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://192.168.1.15:8080/getdirection"]
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://169.254.106.204:8080/getdirection"]
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://192.168.7.102:8080/getdirection"]
+    //NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://169.254.106.204:8080/getdirection"]
                                                            cachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData
                                                        timeoutInterval:10];
     
@@ -100,7 +114,15 @@
     if([responseString  isEqual: @"Left"]){
         [self playLeft];
     }
-    
+    else if([responseString  isEqual: @"Right"]){
+        [self playRight];
+    }
+    else if([responseString  isEqual: @"Forward"]){
+        [self playForward];
+    }
+    else if([responseString  isEqual: @"Stop"]){
+        [self playStop];
+    }
     
     self.displayDirection.text = responseString;
 }
