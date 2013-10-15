7732a044e2c102571197ba5e80ed486a1ccbdfb2
Greg Olmschenk <golmschenk@gmail.com>
2013-10-15 15:31:58 -0400

    Add SettingsTableViewController

---
+++ Crowd Navigation/SettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewController.h
+//  Crowd Navigation
+//
+//  Created by Gregory on 10/15/13.
+//  Copyright (c) 2013 Greg Olmschenk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsTableViewController : UITableViewController
+
+@end
